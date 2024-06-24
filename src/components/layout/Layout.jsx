d15eb0a27acb4253f5e96923ed9b3759560eceaa
vanhvanh <vanhleg2301@gmail.com>
2024-06-24 18:40:00 +0700

    new

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
-import Header from "../header/Header";
+import Header from "../header/header";
 import Footer from "../footer/Footer";
 import { Box, Divider } from "@mui/material";
 import { ThemeProvider } from "@mui/system";
